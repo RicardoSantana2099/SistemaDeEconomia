a8eb569adf84e687d1aa3e69699e65f8623ecd3a
Ricardo Santana <pacheco2099.working@gmail.com>
2023-03-13 14:57:49 -0600

    Purshasing

--- SistemaDeEconomia/Assets/SistemaDeEconomia/Scripts/CharacterShopUI.cs
+++ SistemaDeEconomia/Assets/SistemaDeEconomia/Scripts/CharacterShopUI.cs
@@ -2,17 +2,155 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CharacterShopUI : MonoBehaviour
 {
+    [Header("Layout Settings")]
+    [SerializeField] float itemSpacing = 5f;
+    float itemHeight;
+
+    [Header("UI elements")]
+    [SerializeField] Image selectedCharacterIcon;
+    [SerializeField] Transform ShopMenu;
+    [SerializeField] Transform ShopItemsContainer;
+    [SerializeField] GameObject itemPrefab;
+    [Space(20)]
+    [SerializeField] CharacterShopDatabase characterDB;
+
+    [Space(20)]
     [Header("Shop Events")]
     [SerializeField] GameObject shopUI;
     [SerializeField] Button openShopButton;
     [SerializeField] Button closeShopButton;
 
+    [Space(20)]
+    [Header("Main Menu")]
+    [SerializeField] Image mainMenuCharacterImage;
+    [SerializeField] TMP_Text mainMenuCharacterName;
+
+    int newSelectedItemIndex = 0;
+    int previousSelectedItemIndex = 0;
+
     void Start()
     {
         AddShopEvents();
+        GenerateShopItemsUI();
+        SetSelectedCharacter();
+        SelectItemUI(GameDataManager.GetSelectedCharacterIndex ());
+        ChangePlayerSkin();
+    }
+
+    void SetSelectedCharacter()
+    {
+        int index = GameDataManager.GetSelectedCharacterIndex();
+
+        GameDataManager.SetSelectedCharacter(characterDB.GetCharacter(index), index);
+    }
+
+    void GenerateShopItemsUI()
+    {
+        for(int i = 0; i < GameDataManager.GetAllPurchasedCharacter().Count; i++)
+        {
+            int purchasedCharacterIndex = GameDataManager.GetPurchasedCharacter(i);
+            characterDB.PurchaseCharacter(purchasedCharacterIndex);
+        }
+
+        //Borrar items
+        itemHeight = ShopItemsContainer.GetChild(0).GetComponent<RectTransform>().sizeDelta.y;
+        Destroy(ShopItemsContainer.GetChild(0).gameObject);
+        ShopItemsContainer.DetachChildren();
+
+        //Genera items
+        for(int i = 0; i < characterDB.CharactersCount; i++)
+        {
+            Character character = characterDB.GetCharacter(i);
+            CharacterItemUI uiItem = Instantiate(itemPrefab, ShopItemsContainer).GetComponent<CharacterItemUI>();
+
+            //Mueve el item de posición
+            uiItem.SetItemPosition(Vector2.down * i * (itemHeight + itemSpacing));
+
+            //Nombre de items en la jerarquia
+            uiItem.gameObject.name = "Item" + i + "-" + character.name;
+
+            //Añade información de un item
+            uiItem.SetCharacterName(character.name);
+            uiItem.SetCharacterImage(character.image);
+            uiItem.SetCharacterSpeed(character.speed);
+            uiItem.SetCharacterPower(character.power);
+            uiItem.SetCharacterPrice(character.price);
+
+            if(character.isPurchased)
+            {
+                uiItem.SetCharacterAsPurchased();
+                uiItem.OnItemSelect(i, OnItemSelected);
+            }
+            else
+            {
+                uiItem.SetCharacterPrice(character.price);
+                uiItem.OnItemPurchase(i, OnItemPurchased);
+            }
+            ShopItemsContainer.GetComponent<RectTransform>().sizeDelta =
+                Vector2.up * ((itemHeight + itemSpacing) * characterDB.CharactersCount + itemSpacing);
+        }
+    }
+
+    void ChangePlayerSkin()
+    {
+        Character character = GameDataManager.GetSelectedCharacter();
+        if(character.image != null)
+        {
+            mainMenuCharacterImage.sprite = character.image;
+            mainMenuCharacterName.text = character.name;
+
+            selectedCharacterIcon.sprite = GameDataManager.GetSelectedCharacter().image;
+        }
+    }
+
+    void OnItemSelected(int index)
+    {
+        SelectItemUI(index);
+
+        selectedCharacterIcon.sprite = characterDB.GetCharacter(index).image;
+
+        GameDataManager.SetSelectedCharacter(characterDB.GetCharacter(index), index);
+
+        ChangePlayerSkin();
+    }
+    void SelectItemUI(int itemIndex)
+    {
+        previousSelectedItemIndex = newSelectedItemIndex;
+        newSelectedItemIndex = itemIndex;
+
+        CharacterItemUI prevUiItem = GetItemUI(previousSelectedItemIndex);
+        CharacterItemUI newUiItem = GetItemUI(newSelectedItemIndex);
+
+        prevUiItem.DeselectItem();
+        newUiItem.SelectItem();
+    }
+
+    CharacterItemUI GetItemUI(int index)
+    {
+        return ShopItemsContainer.GetChild(index).GetComponent<CharacterItemUI>();
+    }
+    void OnItemPurchased(int index)
+    {
+        Character character = characterDB.GetCharacter(index);
+        CharacterItemUI uiItem = GetItemUI(index);
+
+        if(GameDataManager.CanSpendCoins(character.price))
+        {
+            GameDataManager.SpendCoins(character.price);
+            GameSharedUI.Instance.UpdateCoinsUIText();
+            characterDB.PurchaseCharacter(index);
+            uiItem.SetCharacterAsPurchased();
+            uiItem.OnItemSelect(index, OnItemSelected);
+            GameDataManager.AddPurchasedCharacter(index);
+        }
+        else
+        {
+            Debug.Log("No enough coins!!");
+        }
     }
 
     void AddShopEvents()
--- SistemaDeEconomia/Assets/SistemaDeEconomia/Scripts/GameDataManager.cs
+++ SistemaDeEconomia/Assets/SistemaDeEconomia/Scripts/GameDataManager.cs
@@ -2,19 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]public class PlayerData
+[System.Serializable]public class CharactersShopData
+{
+    public List<int> purchasedCharactersIndexes = new List<int>();
+}
+[System.Serializable] public class PlayerData
 {
     public int coins = 0;
+    public int selectedCharacterIndex = 0;
 }
 public static class GameDataManager 
 {
     static PlayerData playerData = new PlayerData();
+    static CharactersShopData charactersShopData = new CharactersShopData();
+
+    static Character selectedCharacter;
 
     static GameDataManager()
     {
         LoadPlayerData();
+        LoadCharactersShopData();
     }
 
+    public static Character GetSelectedCharacter()
+    {
+        return selectedCharacter;
+    }
+    public static Character SelectedCharacter()
+    {
+        return selectedCharacter;
+    }
+
+    public static void SetSelectedCharacter(Character character, int index)
+    {
+        selectedCharacter = character;
+        playerData.selectedCharacterIndex = index;
+        SavePlayerData();
+    }
+    public static int GetSelectedCharacterIndex()
+    {
+        return playerData.selectedCharacterIndex;
+    }
+    
     public static int GetCoins()
     {
         return playerData.coins;
@@ -47,4 +76,34 @@ public static class GameDataManager
         BinarySerializer.Save(playerData, "player-data.txt");
         UnityEngine.Debug.Log("<color=magenta>[PlayerData]Saved.</color>");
     }
+
+    public static void AddPurchasedCharacter(int characterIndex)
+    {
+        charactersShopData.purchasedCharactersIndexes.Add(characterIndex);
+        SaveCharactersShopData();
+    }
+
+    public static List<int> GetAllPurchasedCharacter()
+    {
+        return charactersShopData.purchasedCharactersIndexes;
+    }
+
+    public static int GetPurchasedCharacter(int index)
+    {
+        return charactersShopData.purchasedCharactersIndexes[index];
+    }
+
+
+    static void LoadCharactersShopData()
+    {
+       charactersShopData = BinarySerializer.Load<CharactersShopData>("characters-shop-data.txt");
+        UnityEngine.Debug.Log("<color=green>[PlayerData]Loaded.</color>");
+    }
+
+    static void SaveCharactersShopData()
+    {
+        BinarySerializer.Save(charactersShopData, "characters-shop-data.txt");
+        UnityEngine.Debug.Log("<color=magenta>[PlayerData]Saved.</color>");
+    }
+
 }
