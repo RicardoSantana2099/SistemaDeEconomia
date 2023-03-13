594eb451fcbe1582d3eb3893aff1f9fb0d50ccda
Ricardo Santana <pacheco2099.working@gmail.com>
2023-03-13 01:56:54 -0600

    Game
    Se creo el juego y una escena para empezar el juego

---
+++ SistemaDeEconomia/Assets/SistemaDeEconomia/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1f;
+    Rigidbody2D rb;
+    bool isMoving = false;
+    float x, y;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        isMoving = (x != 0f || y != 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            rb.position += new Vector2(x, y) * speed * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        string tag = other.collider.tag;
+
+        if(tag.Equals("coin"))
+        {
+            //Añade monedas
+            Debug.Log("hit coin");
+
+            Destroy(other.gameObject);
+        }
+    }
+}
